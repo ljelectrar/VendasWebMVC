6f0830e0c1307353aa8d2ae8eea8499f12e97454
Leandro Junior Alves Dos Santos <ljelectrar@gmail.com>
2023-11-13 16:30:09 -0300

    Começo do Projetos e testes com o Controller e o Razor

---
+++ VendasWebMvc/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMvc.Models;
+
+namespace VendasWebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Isto é um aplicativo de vendas";
+            ViewData["Professor"] = "Leandro Junior Alves Dos Santos";
+            ViewData["Email"] = "ljelectrar@outlook.com";
+            ViewData["Site"] = "www.ljelectrar.com";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Contato";
+            ViewData["Site"] = "www.ljelectrar.com";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
