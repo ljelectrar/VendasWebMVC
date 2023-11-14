e73fe332cc63459fac8756143620d748a00f553b
Leandro Junior Alves Dos Santos <ljelectrar@gmail.com>
2023-11-14 18:12:23 -0300

    Update Seller e Tratamento de Exceção

---
+++ VendasWebMvc/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace VendasWebMvc.Services.Exceptions
+{
+    public class DbConcurrencyException : ApplicationException
+    {
+        public DbConcurrencyException(string message) : base(message)
+        {
+
+        }
+    }
+}
--- VendasWebMvc/Services/SellerService.cs
+++ VendasWebMvc/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using VendasWebMvc.Data;
 using VendasWebMvc.Models;
 using Microsoft.EntityFrameworkCore;
+using VendasWebMvc.Services.Exceptions;
 
 namespace VendasWebMvc.Services
 {
@@ -38,6 +39,23 @@ namespace VendasWebMvc.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id não encontrada");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
