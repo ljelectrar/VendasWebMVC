e757bd4bb155df7e4f7a89a2e7e6158558957197
Leandro Junior Alves Dos Santos <ljelectrar@gmail.com>
2023-11-14 08:39:33 -0300

    Outras classes e entidades e segunda imigração

--- VendasWebMvc/Data/VendasWebMvcContext.cs
+++ VendasWebMvc/Data/VendasWebMvcContext.cs
@@ -14,6 +14,10 @@ namespace VendasWebMvc.Data
         {
         }
 
-        public DbSet<VendasWebMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
+
     }
 }
