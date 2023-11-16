6eaf9f00ef3b2ad26eec2ff5ece45958e6c3b14d
Leandro Junior Alves Dos Santos <ljelectrar@gmail.com>
2023-11-16 09:21:39 -0300

    Validação no Js e no servidor.

--- VendasWebMvc/Models/Seller.cs
+++ VendasWebMvc/Models/Seller.cs
@@ -8,21 +8,31 @@ namespace VendasWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
         [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O {0} é Obrigatório!")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O tamanho do {0} deve ser entre {2} e {1} caracteres")]
         public string Name { get; set; }
         [DataType(DataType.EmailAddress)]
+
+        [Required(ErrorMessage = "O {0} é Obrigatório!")]
+        [EmailAddress(ErrorMessage = "Insira um email em formato válido!")]
         public string Email { get; set; }
 
         [Display(Name = "Data de Nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "O {0} é Obrigatório!")]
         public DateTime BirthDay { get; set; }
 
         [Display(Name = "Salário Base")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required]
+        [Range(100.0, 10000.0, ErrorMessage = "{0} Deve ser entre {1} e {2}")]
         public double BaseSalary { get; set; }
 
         [Display(Name = "Departamento")]
+        [Required(ErrorMessage = "O {0} é Obrigatório!")]
         public Department department { get; set; }
 
         [Display(Name = "ID do Departamento")]
